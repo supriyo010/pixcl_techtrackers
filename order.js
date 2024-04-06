fd744d64b685ae9ce3cbaa45de78a94e6c99ef9a
Supriyo maji <122034818+supriyo010@users.noreply.github.com>
2024-04-06 18:03:37 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+	{
+		productName: "Mobile",
+		productNumber: "9,06,756",
+		paymentStatus: "Due",
+		status: "Pending",
+	},
+	{
+		productName: "Laptop",
+		productNumber: "98,762",
+		paymentStatus: "Refunded",
+		status: "Declined",
+	},
+	{
+		productName: "Airpods",
+		productNumber: "1,95,345",
+		paymentStatus: "Paid",
+		status: "Active",
+	},
+];
